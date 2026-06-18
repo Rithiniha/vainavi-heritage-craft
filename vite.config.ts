4fb12276ab49c26bf2f4598f1508bc55804840a7
Rithiniha kamalanathan <164982399+Rithiniha@users.noreply.github.com>
2026-06-18 21:22:57 +0530

    vitr config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,10 @@
-import { defineConfig } from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  vite: {
-    base: "/",
-  },
-
-  // 🚨 disable SSR behavior for deployment stability
-  tanstackStart: {
-    server: false,
+  plugins: [react()],
+  base: "/",
+  build: {
+    outDir: "dist/client",
   },
 });
