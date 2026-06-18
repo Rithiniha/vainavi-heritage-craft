a7813d9d9eceae8891253734a8f9f3a4b30632ad
Rithiniha kamalanathan <164982399+Rithiniha@users.noreply.github.com>
2026-06-18 21:41:02 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
-  plugins: [react()],
-  base: "/",
-  build: {
-    outDir: "dist/client",
+  tanstackStart: {
+    server: { entry: "server" },
+
+    // 🚀 Enable Vercel SSR adapter
+    nitro: {
+      preset: "vercel",
+    },
   },
 });
